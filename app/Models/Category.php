a63f1cd5d1d84df9e5d37ea4023561f6c259cbcb
Elder <echumaceroj@ucvvirtual.edu.pe>
2021-05-02 23:27:57 -0500

    blog con laravel y jetstream

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    //relaion 1 a muchos
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'Elder Jose Chumacero Jimenez',
+            'email'=>'echumaceroj@mail.com',
+            'password'=>bcrypt('elder123'),
+        ]);
+        User::factory(99)->create();
+    }
+}
