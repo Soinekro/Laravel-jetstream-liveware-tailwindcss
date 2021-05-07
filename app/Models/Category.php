361c56f81bb6ae854096e825381398fbbcd06929
Elder <echumaceroj@ucvvirtual.edu.pe>
2021-05-07 15:36:33 -0500

    crud completo de categorias y urls con su slug respectivo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
     use HasFactory;
     //relaion 1 a muchos
     public function posts(){
---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'Blog Chumacer')
+
+@section('content_header')
+    <h1>Mostrar Categoria</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
